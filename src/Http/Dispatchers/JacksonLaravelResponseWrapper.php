543570a13b8758f639819247538732c5c295fc71
Thiago Cordeiro <thiagoguetten@gmail.com>
2025-12-14 18:12:19 +0100

    Improved service provider config

--- src/Http/Dispatchers/JacksonLaravelResponseWrapper.php
+++ src/Http/Dispatchers/JacksonLaravelResponseWrapper.php
@@ -2,7 +2,6 @@
 
 namespace Tcds\Io\Jackson\Laravel\Http\Dispatchers;
 
-use Illuminate\Config\Repository as ConfigRepository;
 use Illuminate\Http\JsonResponse;
 use Tcds\Io\Generic\Reflection\Type\Parser\TypeParser;
 use Tcds\Io\Generic\Reflection\Type\ReflectionType;
@@ -11,21 +10,16 @@ use Tcds\Io\Jackson\ObjectMapper;
 
 readonly class JacksonLaravelResponseWrapper
 {
-    /** @var array<string, array> */
-    private array $config;
-
-    public function __construct(
-        private ObjectMapper $mapper,
-        ConfigRepository $config,
-    ) {
-        $this->config = $config->get('serializer.classes', []);
+    public function __construct(private ObjectMapper $mapper, private array $classes)
+    {
     }
 
     public function respond(mixed $response, string $returnType): mixed
     {
         [$type, $generics] = TypeParser::getGenericTypes($returnType);
         $type = $type === 'mixed' && is_object($response) ? $response::class : $type;
-        $listType = ReflectionType::isList($type) ? $generics[0] ?? 'mixed' : 'mixed';
+        $isList = ReflectionType::isList($type);
+        $listType = $isList ? $generics[0] ?? 'mixed' : 'mixed';
 
         return match (true) {
             $response instanceof JacksonLaravelResponse => new JsonResponse(
@@ -33,7 +27,8 @@ readonly class JacksonLaravelResponseWrapper
                 status: $response->getStatusCode(),
                 headers: $response->headers->all() ?: [],
             ),
-            isset($this->config[$type]), ReflectionType::isList($type) && isset($this->config[$listType]) => $this->mapper->writeValue($response),
+            isset($this->classes[$type]),
+            ($isList && isset($this->classes[$listType])) => $this->mapper->writeValue($response),
             default => $response,
         };
     }
--- src/Providers/JacksonLaravelObjectMapperProvider.php
+++ src/Providers/JacksonLaravelObjectMapperProvider.php
@@ -9,7 +9,7 @@ use Tcds\Io\Jackson\ArrayObjectMapper;
 use Tcds\Io\Jackson\JsonObjectMapper;
 use Tcds\Io\Jackson\Laravel\Http\Dispatchers\JacksonLaravelCallableDispatcher;
 use Tcds\Io\Jackson\Laravel\Http\Dispatchers\JacksonLaravelControllerDispatcher;
-use Tcds\Io\Jackson\Laravel\Http\JacksonLaravelRequestDispatcher;
+use Tcds\Io\Jackson\Laravel\Http\Dispatchers\JacksonLaravelResponseWrapper;
 use Tcds\Io\Jackson\ObjectMapper;
 
 class JacksonLaravelObjectMapperProvider extends ServiceProvider
@@ -28,8 +28,13 @@ class JacksonLaravelObjectMapperProvider extends ServiceProvider
         $this->app->singleton(ArrayObjectMapper::class, fn() => $arrayMapper);
         $this->app->singleton(JsonObjectMapper::class, fn() => new JsonObjectMapper(typeMappers: $classes));
         $this->app->singleton(ObjectMapper::class, fn() => $arrayMapper);
+
         $this->app->singleton(CallableDispatcher::class, JacksonLaravelCallableDispatcher::class);
         $this->app->singleton(ControllerDispatcher::class, JacksonLaravelControllerDispatcher::class);
-        $this->app->singleton(JacksonLaravelRequestDispatcher::class, JacksonLaravelRequestDispatcher::class);
+
+        $this->app->singleton(JacksonLaravelResponseWrapper::class, fn() => new JacksonLaravelResponseWrapper(
+            mapper: $arrayMapper,
+            classes: $classes,
+        ));
     }
 }
