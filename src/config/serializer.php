d5e82f3b638f4140a888ba0f68e7fc90c8df4b3d
Thiago Cordeiro <thiagoguetten@gmail.com>
2026-01-01 15:19:37 +0100

    Renamed trace to path

--- src/config/serializer.php
+++ src/config/serializer.php
@@ -5,8 +5,8 @@ use Tcds\Io\Jackson\ObjectMapper;
 /**
  * @returns array{
  *     classes: array<class-string, array{
- *         reader?: callable(mixed $value, string $type, ObjectMapper $mapper, array $trace): mixed,
- *         writer?: callable(mixed $data, string $type, ObjectMapper $mapper, array $trace): mixed,
+ *         reader?: callable(mixed $value, string $type, ObjectMapper $mapper, array $path): mixed,
+ *         writer?: callable(mixed $data, string $type, ObjectMapper $mapper, array $path): mixed,
  *     }>,
  *     params?: callable(ObjectMapper $mapper): array
  * }
