990283b828ab70cf1e6ed41d0848e8f19357ae6e
Thiago Cordeiro <thiagoguetten@gmail.com>
2026-01-14 09:16:08 +0100

    Improved config example

--- src/config/jackson.php
+++ src/config/jackson.php
@@ -1,18 +1,29 @@
 <?php
 
 use Psr\Container\ContainerInterface;
+use Tcds\Io\Jackson\Node\Reader;
+use Tcds\Io\Jackson\Node\StaticReader;
+use Tcds\Io\Jackson\Node\StaticWriter;
+use Tcds\Io\Jackson\Node\Writer;
 use Tcds\Io\Jackson\ObjectMapper;
 
 /**
  * @returns array{
  *     mappers: array<class-string, array{
- *         reader?: callable(mixed $value, string $type, ObjectMapper $mapper, array $path): mixed,
- *         writer?: callable(mixed $data, string $type, ObjectMapper $mapper, array $path): mixed,
+ *         reader?: Reader<mixed>|StaticReader<mixed>|Closure(mixed $data, string $type, ObjectMapper $mapper, list<string> $path): mixed,
+ *         writer?: Writer<mixed>|StaticWriter<mixed>|Closure(mixed $data, string $type, ObjectMapper $mapper, list<string> $path): mixed,
  *     }>,
  *     params?: callable(ContainerInterface $container, ObjectMapper $mapper): array
  * }
  */
 return [
-    'mappers' => [],
-    'params' => fn () => [],
+    'mappers' => [
+        // 'class-string' => [
+        //    'reader' => fn(mixed $data) => new class-string($data[...], $data[...]),
+        //    'writer' => fn(class-string $data) => [...],
+        //],
+    ],
+    'params' => fn () => [
+        // 'userId' => Auth::user()->id
+    ],
 ];
