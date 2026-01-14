a66efe675b1fb4c94a58a6c8d61cf9c691d37904
Thiago Cordeiro <thiagoguetten@gmail.com>
2026-01-14 19:21:19 +0100

    Add collection mapper

--- src/Providers/JacksonLaravelObjectMapperProvider.php
+++ src/Providers/JacksonLaravelObjectMapperProvider.php
@@ -5,11 +5,13 @@ namespace Tcds\Io\Jackson\Laravel\Providers;
 use Carbon\Laravel\ServiceProvider;
 use Illuminate\Routing\Contracts\CallableDispatcher;
 use Illuminate\Routing\Contracts\ControllerDispatcher;
+use Illuminate\Support\Collection;
 use Tcds\Io\Jackson\ArrayObjectMapper;
 use Tcds\Io\Jackson\JsonObjectMapper;
 use Tcds\Io\Jackson\Laravel\Http\Dispatchers\JacksonLaravelCallableDispatcher;
 use Tcds\Io\Jackson\Laravel\Http\Dispatchers\JacksonLaravelControllerDispatcher;
 use Tcds\Io\Jackson\Laravel\Http\Dispatchers\JacksonLaravelResponseWrapper;
+use Tcds\Io\Jackson\Laravel\Mappers\CollectionMapper;
 use Tcds\Io\Jackson\ObjectMapper;
 
 class JacksonLaravelObjectMapperProvider extends ServiceProvider
@@ -23,16 +25,17 @@ class JacksonLaravelObjectMapperProvider extends ServiceProvider
     public function register(): void
     {
         $mappers = config('jackson.mappers', []);
-        $arrayMapper = new ArrayObjectMapper(typeMappers: $mappers);
 
-        $this->app->singleton(ArrayObjectMapper::class, fn () => $arrayMapper);
-        $this->app->singleton(JsonObjectMapper::class, fn () => new JsonObjectMapper(typeMappers: $mappers));
-        $this->app->singleton(ObjectMapper::class, fn () => $arrayMapper);
+        $arrayMapper = new ArrayObjectMapper(typeMappers: [$mappers, ...CollectionMapper::get(Collection::class)]);
+
+        $this->app->singleton(ArrayObjectMapper::class, fn() => $arrayMapper);
+        $this->app->singleton(JsonObjectMapper::class, fn() => new JsonObjectMapper(typeMappers: $mappers));
+        $this->app->singleton(ObjectMapper::class, fn() => $arrayMapper);
 
         $this->app->singleton(CallableDispatcher::class, JacksonLaravelCallableDispatcher::class);
         $this->app->singleton(ControllerDispatcher::class, JacksonLaravelControllerDispatcher::class);
 
-        $this->app->singleton(JacksonLaravelResponseWrapper::class, fn () => new JacksonLaravelResponseWrapper(
+        $this->app->singleton(JacksonLaravelResponseWrapper::class, fn() => new JacksonLaravelResponseWrapper(
             mapper: $arrayMapper,
             mappers: $mappers,
         ));
