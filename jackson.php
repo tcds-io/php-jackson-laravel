969130654e5fceb2b4a104457ea8707e629599d0
Thiago Cordeiro <thiagoguetten@gmail.com>
2026-04-10 16:20:47 +0200

    Allow custom error handler

--- jackson.php
+++ jackson.php
@@ -1,6 +1,10 @@
 <?php
 
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
 use Psr\Container\ContainerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Tcds\Io\Jackson\Exception\UnableToParseValue;
 use Tcds\Io\Jackson\Node\Reader;
 use Tcds\Io\Jackson\Node\StaticReader;
 use Tcds\Io\Jackson\Node\StaticWriter;
@@ -9,6 +13,9 @@ use Tcds\Io\Jackson\ObjectMapper;
 
 /**
  * @returns array{
+ *     errors?: array{
+ *         request?: Closure(UnableToParseValue $e): Throwable
+ *     },
  *     mappers: array<class-string, array{
  *         reader?: Reader<mixed>|StaticReader<mixed>|Closure(mixed $data, string $type, ObjectMapper $mapper, list<string> $path): mixed,
  *         writer?: Writer<mixed>|StaticWriter<mixed>|Closure(mixed $data, string $type, ObjectMapper $mapper, list<string> $path): mixed,
@@ -17,13 +24,22 @@ use Tcds\Io\Jackson\ObjectMapper;
  * }
  */
 return [
+    'errors' => [
+        // 'request' => fn(UnableToParseValue $e) => new HttpResponseException(
+        //     new JsonResponse([
+        //         'message' => $e->getMessage(),
+        //         'expected' => $e->expected,
+        //         'given' => $e->given,
+        //     ], Response::HTTP_BAD_REQUEST),
+        // ),
+    ],
     'mappers' => [
         // 'class-string' => [
         //    'reader' => fn(mixed $data) => new class-string($data[...], $data[...]),
         //    'writer' => fn(class-string $data) => [...],
         //],
     ],
-    'params' => fn () => [
+    'params' => fn() => [
         // 'userId' => Auth::user()->id
     ],
 ];
--- src/JacksonConfig.php
+++ src/JacksonConfig.php
@@ -3,37 +3,60 @@
 namespace Tcds\Io\Jackson\Laravel;
 
 use Closure;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
 use Psr\Container\ContainerInterface as Container;
+use Symfony\Component\HttpFoundation\Response;
 use Tcds\Io\Generic\Reflection\ReflectionFunction;
 use Tcds\Io\Generic\Reflection\Type\Parser\TypeParser;
 use Tcds\Io\Generic\Reflection\Type\ReflectionType;
+use Tcds\Io\Jackson\Exception\UnableToParseValue;
 use Tcds\Io\Jackson\ObjectMapper;
+use Throwable;
 
 /**
  * @phpstan-import-type TypeMapper from ObjectMapper
  * @phpstan-type Mappers array<string, TypeMapper>
  * @phpstan-type CustomParams Closure(...$args): array<string, mixed>
+ * @phpstan-type RequestErrorHandler Closure(UnableToParseValue $e): Throwable
  */
 readonly class JacksonConfig
 {
     /**
      * @param Mappers $mappers
+     * @param CustomParams $customParams
+     * @param ?RequestErrorHandler $requestErrorHandler
      */
-    public function __construct(public array $mappers, private Closure $customParams)
-    {
+    public function __construct(
+        public array $mappers,
+        private Closure $customParams,
+        private ?Closure $requestErrorHandler,
+    ) {
     }
 
     public static function fromConfigFile(string $file): JacksonConfig
     {
-        /** @var array{ mappers?: Mappers, params?: CustomParams } $config */
+        /** @var array{
+         *     errors?: array{
+         *        request?: RequestErrorHandler
+         *     },
+         *     mappers?: Mappers,
+         *     params?: CustomParams,
+         * } $config
+         */
         $config = file_exists($file)
             ? require $file
             : [];
 
+        $errors = $config['errors'] ?? [];
         $mappers = $config['mappers'] ?? [];
         $customParams = $config['params'] ?? fn() => [];
 
-        return new self($mappers, $customParams);
+        return new self(
+            mappers: $mappers,
+            customParams: $customParams,
+            requestErrorHandler: $errors['request'] ?? null,
+        );
     }
 
     public function readable(string $type): bool
@@ -83,5 +106,18 @@ readonly class JacksonConfig
             'container' => $container,
             'mapper' => $mapper,
         ]);
+    }
+
+    public function handleRequestError(UnableToParseValue $e): Throwable
+    {
+        $handler = $this->requestErrorHandler ?? fn(UnableToParseValue $e) => new HttpResponseException(
+            new JsonResponse([
+                'message' => $e->getMessage(),
+                'expected' => $e->expected,
+                'given' => $e->given,
+            ], Response::HTTP_BAD_REQUEST),
+        );
+
+        return $handler($e);
     }
 }
